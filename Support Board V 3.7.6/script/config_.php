7460fc29eca985b12ea5346b9bde04c5312e7b7c
Afenuvon Adebayo Koku <adebayorafenuvon@gmail.com>
2019-04-14 01:44:15 +0100

    Add Support Board V 3.7.6/script/config_.php (timestamp: 2019-04-14 01:44:15)

---
+++ Support Board V 3.7.6/script/config_.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * ==========================================================
+ * INITIAL CONFIGURATION FILE
+ * ==========================================================
+ *
+ * Cloud configuration file for Support Board
+ *
+ */
+
+define('SUPER_EMAIL', 'AAA');
+define('SUPER_PASSWORD', 'AAA');
+
+define('CLOUD_PUSHER_ID', 0000);
+define('CLOUD_PUSHER_KEY', 'AAA');
+define('CLOUD_PUSHER_SECRET', 'AAA');
+define('CLOUD_PUSHER_CLUSTER', 'AAA');
+
+define('CLOUD_TWILIO_SID', '');
+define('CLOUD_TWILIO_TOKEN', '');
+define('CLOUD_TWILIO_SENDER', '');
+
+define('CLOUD_SMTP_HOST', 'AAA');
+define('CLOUD_SMTP_USERNAME', 'AAA');
+define('CLOUD_SMTP_PASSWORD', 'AAA');
+define('CLOUD_SMTP_PORT', 587);
+define('CLOUD_SMTP_SENDER', 'AAA');
+define('CLOUD_SMTP_SENDER_NAME', 'AAA');
+
+define('CLOUD_DB_NAME', 'AAA');
+define('CLOUD_DB_USER', 'AAA');
+define('CLOUD_DB_PASSWORD', 'AAA');
+define('CLOUD_DB_HOST', 'localhost');
+define('CLOUD_URL', 'https://DOMAIN');
+
+define('SB_CLOUD', true);
+define('SB_CLOUD_KEY', 'AAA');
+define('SB_CLOUD_PATH', '/var/www/vhosts/DOMAIN/httpdocs');
+define('SB_CLOUD_BRAND_LOGO', 'https://DOMAIN/custom/logo.svg');
+define('SB_CLOUD_BRAND_LOGO_LINK', 'https://DOMAIN');
+define('SB_CLOUD_BRAND_ICON', 'https://DOMAIN/custom/icon.svg');
+define('SB_CLOUD_BRAND_ICON_PNG', 'https://DOMAIN/custom/icon.png');
+define('SB_CLOUD_BRAND_NAME', 'BBB');
+define('SB_CLOUD_MANIFEST_URL', 'https://DOMAIN/manifest.json');
+define('SB_CLOUD_MEMBERSHIP_TYPE', 'messages-agents'); //messages, users, agents
+
+define('PAYMENT_PROVIDER', 'stripe'); //rapyd, verifone
+
+define('STRIPE_SECRET_KEY', 'XXX');
+define('STRIPE_PRODUCT_ID', 'XXX');
+define('STRIPE_CURRENCY', 'usd');
+
+define('ONESIGNAL_APP_ID', 'AAA');
+define('ONESIGNAL_API_KEY', 'AAA');
+
+define('ENVATO_PURCHASE_CODE' ,'AAA');
+
+define('OPEN_EXCHANGE_RATE_APP_ID', 'AAA');
+
+//define('GOOGLE_CLIENT_ID', '');
+//define('GOOGLE_CLIENT_SECRET', '');
+
+//define('WHATSAPP_APP_ID', '');
+//define('WHATSAPP_APP_SECRET', '');
+//define('WHATSAPP_CONFIGURATION_ID', '');
+//define('WHATSAPP_APP_TOKEN', '');
+//define('WHATSAPP_VERIFY_TOKEN', '');
+
+//define('MESSENGER_APP_ID', '');
+//define('MESSENGER_APP_SECRET', '');
+//define('MESSENGER_CONFIGURATION_ID', '');
+//define('MESSENGER_VERIFY_TOKEN', '');
+//define('MESSENGER_APP_TOKEN', '');
+
+//define('OPEN_AI_KEY', '');
+
+//define('RAZORPAY_KEY_ID', '');
+//define('RAZORPAY_KEY_SECRET', '');
+//define('RAZORPAY_CURRENCY', '');
+
+//define('RAPYD_ACCESS_KEY', '');
+//define('RAPYD_SECRET_KEY', '');
+//define('RAPYD_TEST_MODE', );
+//define('RAPYD_CURRENCY', '');
+//define('RAPYD_COUNTRY', '');
+
+//define('VERIFONE_SECRET_WORD', '');
+//define('VERIFONE_SECRET_KEY', '');
+//define('VERIFONE_MERCHANT_ID', '');
+//define('VERIFONE_CURRENCY', '');
+
+//define('SB_CLOUD_AWS_S3', ['amazon-s3-access-key' => '', 'amazon-s3-secret-access-key' => '', 'amazon-s3-bucket-name' => '', 'amazon-s3-region' => '']);
+//define('SB_CLOUD_DOCS', '');
+//define('STRIPE_PRODUCT_ID_WHITE_LABEL', '');
+//define('CLOUD_IP', '');
+//define('SB_CLOUD_DEFAULT_LANGUAGE_CODE', 'zh');
+//define('SB_CLOUD_DEFAULT_RTL', true);
+
+?>
